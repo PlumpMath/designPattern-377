d78eb01e5e697fbe1d27f7716c0eec40fe556f0e
cle009 <clement009@hotmail.com>
2016-12-02 20:10:06 +0800

    add two classes

---
+++ simplefactory/src/AddOperator.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/11/25.
+ */
+public class AddOperator implements Operator {
+
+	@Override
+	public double calculate(double operA, double operB) {
+
+		return operA + operB;
+	}
+}
